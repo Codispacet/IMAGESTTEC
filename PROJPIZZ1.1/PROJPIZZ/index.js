41783f2aada081352fc51641123b273959539a18
Codispacet <codiinspace@hotmail.com>
2025-08-07 12:12:59 -0300

    Add files via upload

---
+++ PROJPIZZ1.1/PROJPIZZ/index.js
@@ -0,0 +1,14 @@
+
+function abricard(evt, cardname) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cardname).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
